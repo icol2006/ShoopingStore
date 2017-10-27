ded12ac1bd1416e7f65c995a7fc67ce95651a6cd
maykokl <maykolxx@gmail.com>
2017-10-26 21:26:53 -0600

    26/10/2017

---
+++ MvcAffableBean/DAL/MvcAffableBeanContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MvcAffableBean.Models;
+using System.Data.Entity.Validation;
+
+namespace MvcAffableBean.DAL
+{
+    public class MvcAffableBeanContext:DbContext
+
+    {
+        public MvcAffableBeanContext() : base("MvcAffableBean")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<CustomerOrder> CustomerOrders { get; set; }
+
+        public DbSet<OrderedProduct> Orderedproducts { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<MvcAffableBeanContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
